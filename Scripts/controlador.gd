a05a2d92b10df1d2e53e1482b5f01c88d6bfa8bf
Nevercreature <hmh-master@hotmail.com>
2024-11-26 18:28:11 +0100

    Buen funcionamiento inicial reinicio bola

--- Scripts/controlador.gd
+++ Scripts/controlador.gd
@@ -17,6 +17,15 @@ var canvas_layer
 
 var valor_zoom: int
 
+var botonL1
+var botonR1
+
+var bola
+
+var vidas
+
+var panel_soltar_bola
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	separador = "." # Replace with function body.
@@ -32,6 +41,14 @@ func _ready() -> void:
 	nodo_sprite_moneda = get_node("../CanvasLayer/MonedaInterfaz")
 
 	canvas_layer = get_node("../CanvasLayer")
+	
+	botonL1 = get_node("%ButtonIzqLanzar")
+	botonR1 = get_node("%ButtonDerLanzar")
+	
+	bola = get_node("%Bola")
+	panel_soltar_bola = get_node("%PanelSoltarBola")
+	
+	vidas = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -42,7 +59,7 @@ func _process(delta: float) -> void:
 		valor_zoom = 1
 
 	
-	puntos_string = formatea_numeros_con_ceros(puntos_entero)
+	puntos_string = formatea_numeros_con_puntos(puntos_entero)
 	nodo_puntos.text = puntos_string
 	nodo_puntos_sombra.text = puntos_string
 	nodo_puntos_sombra2.text = puntos_string
@@ -61,10 +78,30 @@ func _process(delta: float) -> void:
 	nodo_sprite_moneda.scale.x = valor_zoom
 	nodo_sprite_moneda.scale.y = valor_zoom
 	
+	#control_de_la_bola()
 	
-	
-	#print(nodo_puntos_sombra.name)
+func formatea_numeros_con_puntos(puntos_entero: int) -> String:
+	var str_number = str(puntos_entero)
+	var parts = str_number.split(".")  # Divide entre parte entera y decimal
+	var integer_part = parts[0]
+	var decimal_part
+	if parts.size() > 1:
+		decimal_part = "." + parts[1]
+	else:
+		decimal_part = ""
 
+	var formatted_integer = ""
+	var counter = 0
+
+	# Itera desde el final al principio para insertar los puntos
+	for i in range(integer_part.length() - 1, -1, -1):
+		formatted_integer = integer_part[i] + formatted_integer
+		counter+=1
+		if counter % 3 == 0 and i > 0:
+			formatted_integer = "." + formatted_integer
+	return formatted_integer + decimal_part
+	#print(nodo_puntos_sombra.name)
+'''
 func formatea_numeros_con_ceros(number: int) -> String:
 	var number_str = str(number)
 	var zeros_needed = max(0, 9 - number_str.length())
@@ -74,3 +111,28 @@ func formatea_numeros_con_ceros(number: int) -> String:
 	var padded = padding + number_str
 	var formatted = padded.substr(0, 3)+separador+padded.substr(3, 3)+separador+padded.substr(6, 3)
 	return formatted
+'''
+func teletransportar_bola(nueva_posicion: Vector2):
+	# Cambiamos el estado de "sleeping" para evitar que se quede congelado. Y que no sea visible mientras se transporta
+	#bola.visible = true
+	bola.global_position.y = nueva_posicion.y #bola.global_position.y = nueva_posicion.y
+	bola.global_position.x = nueva_posicion.x #bola.global_position.x = nueva_posicion.x
+
+
+	####################################################################### Esto es el reseteo de la bola
+	#await get_tree().create_timer(1).timeout #await get_tree().create_timer(0.5).timeout
+	bola.gravity_scale = bola.gravedad
+	#bola.freeze = false
+	bola.visible = true
+	####################################################################
+
+func _input(event):
+	if event.is_action_pressed("L1"):
+		teletransportar_bola(Vector2(-180.5,140))
+		#panel_soltar_bola.freeze = true
+		panel_soltar_bola.visible = false
+
+	if event.is_action_pressed("R1"):
+		teletransportar_bola(Vector2(+180.5,140))
+		#panel_soltar_bola.freeze = true
+		panel_soltar_bola.visible = false
--- camera_2d.gd
+++ camera_2d.gd
@@ -3,18 +3,34 @@ extends Camera2D
 
 
 func _ready() -> void:
-	_ajustar_Zoom() 
+	_ajustar_Zoom_pixel_perfect() 
 #---------------------------------------------------------------------------------------------------------------------
 func _process(delta: float) -> void:
 	
-	_ajustar_Zoom()
+	_ajustar_Zoom_pixel_perfect()
 	
 	_seguir_a_la_bola()
 
 #---------------------------------------------------------------------------------------------------------------------
-func _ajustar_Zoom(): #Esto ajusta el zoom a un múltiplo de 360. El Full HD serían 1080 qye es una tercera parte
-	var valor_zoom = round(get_viewport().size.y / 360)
+func _ajustar_Zoom_pixel_perfect(): #Esto ajusta el zoom a un múltiplo de 360. El Full HD serían 1080 qye es una tercera parte
+	
+	var valor_y_de_escena = float(360)
+	
+	var valor_zoom = round(get_viewport().size.y / valor_y_de_escena)
+
 	zoom = Vector2(valor_zoom, valor_zoom)
+
+func _ajustar_Zoom_dinamico(): #Esto ajusta el zoom a un múltiplo de 360. El Full HD serían 1080 qye es una tercera parte
+	
+
+	#Esto es para zoom dinamico
+	var valor_y_de_escena = float(360)
+	
+	var valor_zoom = get_viewport().size.y / valor_y_de_escena
+	print("La proporcion :",get_viewport().size.y / valor_y_de_escena)
+	print("Viewport en Y :",get_viewport().size.y)
+	zoom = Vector2(valor_zoom, valor_zoom)
+
 #--------------------------------------------------------------------------------------------------------------------	
 func _seguir_a_la_bola(): #Si la bola va más allá de los límites puestos la cámara no se mueve para no salirse del area mostrable
 	#Aquí se pilla la bola
@@ -36,11 +52,11 @@ func sacude_camara():
 	await get_tree().create_timer(tiempo_espera).timeout 
 	position = Vector2(posicion_actual.x+5,posicion_actual.y+3)
 	await get_tree().create_timer(tiempo_espera).timeout 
-	position = Vector2(posicion_actual.x+4,posicion_actual.y-2)
+	position = Vector2(posicion_actual.x+5,posicion_actual.y-2)
 	await get_tree().create_timer(tiempo_espera).timeout 
 	position = Vector2(posicion_actual.x,posicion_actual.y)
 	await get_tree().create_timer(tiempo_espera).timeout 
-	position = Vector2(posicion_actual.x-6,posicion_actual.y+3)
+	position = Vector2(posicion_actual.x-6,posicion_actual.y+6)
 	await get_tree().create_timer(tiempo_espera).timeout 
 	position = Vector2(posicion_actual.x-4,posicion_actual.y-2)
 	await get_tree().create_timer(tiempo_espera).timeout 
@@ -48,7 +64,13 @@ func sacude_camara():
 	await get_tree().create_timer(tiempo_espera).timeout 
 	position = Vector2(posicion_actual.x-1,posicion_actual.y+6)
 	await get_tree().create_timer(tiempo_espera).timeout 
-	position = Vector2(posicion_actual.x+4,posicion_actual.y+3)
+	position = Vector2(posicion_actual.x+7,posicion_actual.y+3)
+	await get_tree().create_timer(tiempo_espera).timeout
+	position = Vector2(posicion_actual.x+5,posicion_actual.y+3)
 	await get_tree().create_timer(tiempo_espera).timeout 
+	position = Vector2(posicion_actual.x+5,posicion_actual.y-2)
+	await get_tree().create_timer(tiempo_espera).timeout 
+	position = Vector2(posicion_actual.x,posicion_actual.y)
+	await get_tree().create_timer(tiempo_espera).timeout  
 	position = posicion_actual
 	
